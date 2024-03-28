a9756dbd259a7bc1a369dd7b7e156e62121836fc
Nebula <152747126+Nebulized@users.noreply.github.com>
2024-03-28 23:29:48 +0100

    Update SM64coopdx Auto Compiler.py

--- SM64coopdx Auto Compiler.py
+++ SM64coopdx Auto Compiler.py
@@ -7,11 +7,11 @@ import re
 #Some features worth adding is argument errors like the jobs variable
 print("Example input: make --jobs=4 TARGET_BITS=64 DISCORD_SDK=1 COOPNET=1 HEADLESS=0 RENDER_API=GL WINDOW_API=SDL2")
 use_default_args = input("USE_DEFAULT_ARGUMENTS [Y|N] Use the default arguments ", )
-jobs = input("--jobs[1,2,3,4,...] Jobs amount, may speed up compilation. ", )
-if not re.match(r'^\d+$', jobs):
-    print("error info: non valid arguments for --jobs")
-    sys.exit()
 if use_default_args == "Y":
+    jobs = input("--jobs[1,2,3,4,...] Jobs amount, may speed up compilation. ", )
+    if not re.match(r'^\d+$', jobs):
+        print("error info: non valid arguments for --jobs")
+        sys.exit()
     target_bits = "64"
     discord_sdk = "1"
     coopnet = "1"
@@ -19,12 +19,19 @@ if use_default_args == "Y":
     render_api = "GL"
     window_api = "SDL2"
 elif use_default_args == "N":
+    jobs = input("--jobs[1,2,3,4,...] Jobs amount, may speed up compilation. ", )
+    if not re.match(r'^\d+$', jobs):
+        print("error info: non valid arguments for --jobs")
+        sys.exit()
     target_bits = input("TARGET_BITS [32|64] Compile to 32-bit or 64-bit. ", )
     discord_sdk = input("DISCORD_SDK [0|1] Enable or disable Discord Game SDK. ", )
     coopnet = input("COOPNET [0|1] Enable or disable the CoopNet networking system. ", )
     headless = input("HEADLESS [0|1] Enable or disable headless mode (meant for servers). ", )
     render_api = input("RENDER_API [GL|GL_LEGACY|D3D11|D3D12|DUMMY] Sets the rendering API. ", )
     window_api = input("WINDOW_API [SDL1|SDL2|DXGI|DUMMY] Sets the window API. ", )
+else:
+    print("error info: non valid arguments for use_default_arguments")
+    sys.exit()
 
 
 final_jobs = " --jobs="+jobs
